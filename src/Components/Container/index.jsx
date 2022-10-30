2552f1246536920f525da74221692cbd0e32cdbe
Ítalo Magno <italomagnov@gmail.com>
2022-10-30 12:41:22 -0400

    refatoração do código e media query

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-
 import Titulo from "../Titulo";
 import Conta from "../Conta";
+import styled from 'styled-components'
+import { fundoClaro } from '../UI/Variaveis'; 
 
-const Container = () => {
+const Container = styled.div`
+  background-color: ${fundoClaro};
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  padding-bottom: 32px;
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`
+
+export default () => {
   return (
-    <div className="container">
-      <Titulo>Olá Fulano!</Titulo>
-      <section className="conteudo">
+    <Container>
+      <Titulo>Olá Rory</Titulo>
+      <Conteudo>
         <Conta />
-      </section>
-    </div>
+      </Conteudo>
+    </Container>
   );
 };
 
-export default Container;
+
